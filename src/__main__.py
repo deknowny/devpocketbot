1208a4d25f5e90d6c58e6d1f406d16963f5fa411
Rhinik <deknowny@gmail.com>
2021-06-12 16:40:54 +0300

    add run into code

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,13 @@
+import uvicorn
 import fastapi
 
 
 app = fastapi.FastAPI()
 
 
-@app.route("/redirect-endpoint")
+@app.get("/redirect-endpoint")
 async def redirect_endpoint(code: str):
     print(code)
+
+
+uvicorn.run(app, host="185.119.58.249", port=9000)
